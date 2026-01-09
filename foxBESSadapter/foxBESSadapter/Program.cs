c67cf601a5e1fca7262a9866232debac7c4fcaa9
FlexCHESS <tim.farnham@toshiba.eu>
2026-01-09 18:21:08 +0000

    Update Program.cs

--- foxBESSadapter/foxBESSadapter/Program.cs
+++ foxBESSadapter/foxBESSadapter/Program.cs
@@ -16,7 +16,11 @@ namespace foxBESSadapter
     public class Program
     {
 
-        public static String urlprefix = "https://preprodapim.umbrellaiot.com:9095/aas/3.0";
+        public static string key = System.Environment.GetEnvironmentVariable("CONF", EnvironmentVariableTarget.Process);
+        public static string uudexUser = System.Environment.GetEnvironmentVariable("UUDEX_USER", EnvironmentVariableTarget.Process);
+        public static string uudexPass = System.Environment.GetEnvironmentVariable("UUDEX_PASS", EnvironmentVariableTarget.Process);
+
+        public static String urlprefix = "http://aasserver.default.svc/api/v3.0"; 
         public static void Main(string[] args)
         {
 
